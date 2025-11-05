f927e91ab9da2cd4f19b61e06ee5f04efa90ca6e
John Krazy <gc.john.dcg@gmail.com>
2025-11-05 00:44:40 -0600

    first

---
+++ src/js/db.js
@@ -0,0 +1,310 @@
+export const db = {
+    usuarios: [
+        {
+            name: "Jonathan",
+            apellido: "Grajeda",
+            items: [1234, 4321, 2341, 3421, 2431],
+        },
+    ],
+    editoriales: [
+        {
+            name: "Salvat",
+            image: "",
+            items: [
+                {
+                    name: "Mini Libros Disney",
+                    cover: "",
+                    editions: [
+                        {
+                            id: 1234,
+                            description: "Bolt",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_805306-MLA54182966574_032023-F.webp",
+                            code: "43523546",
+                            edition: "14",
+                        },
+                        {
+                            id: 2341,
+                            description: "Ratatouille",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_958516-MLA91957044829_092025-F.webp",
+                            code: "",
+                            edition: "15",
+                        },
+                        {
+                            description: "Chicken Little",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_838506-MLM54275956467_032023-F.webp",
+                            code: "",
+                            edition: "16",
+                        },
+                    ],
+                },
+                {
+                    name: "Mini Libros Disney 2024",
+                    cover: "",
+                    editions: [
+                        {
+                            description: "Clásicos Con Mickey",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_732362-MLM80406051682_112024-F.webp",
+                            code: "",
+                            edition: "104",
+                        },
+                        {
+                            description: "Mas Sobre Cenicienta",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_672033-MLM80544897822_112024-F.webp",
+                            code: "",
+                            edition: "103",
+                        },
+                        {
+                            description: "Los Increibles de compras con mamá",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_605295-MLA81002623229_112024-F.webp",
+                            code: "",
+                            edition: "102",
+                        },
+                    ],
+                },
+                {
+                    name: "Mini Libros Disney Julio Verne",
+                    cover: "",
+                    editions: [
+                        {
+                            description: "La Vuelta Al Mundo en 80 días",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_994710-MLM84964165672_052025-F.webp",
+                            code: "",
+                            edition: "1",
+                        },
+                        {
+                            description: "Viaje Al Centro De La Tierra",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_912637-MLM85647252441_062025-F.webp",
+                            code: "",
+                            edition: "2",
+                        },
+                        {
+                            description: "Cinco Semanas En Globo",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_937811-MLM86024576820_062025-F.webp",
+                            code: "",
+                            edition: "3",
+                        },
+                    ],
+                },
+                {
+                    name: "Novelas Disney",
+                    cover: "",
+                    editions: [
+                        {
+                            description: "La Bella Y La Bestia",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_632867-MLA83395110043_032025-F.webp",
+                            code: "",
+                            edition: "1",
+                        },
+                        {
+                            description: "Frozen",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_667959-MLA83565793095_042025-F.webp",
+                            code: "",
+                            edition: "2",
+                        },
+                        {
+                            description: "Blanca Nievas",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_917482-MLM83506912455_042025-F.webp",
+                            code: "",
+                            edition: "3",
+                        },
+                        {
+                            description: "El Rey Leon",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_837431-MLA83878553081_042025-F.webp",
+                            code: "",
+                            edition: "4",
+                        },
+                    ],
+                },
+                {
+                    name: "Mustag GT 1968",
+                    cover: "",
+                    editions: [
+                        {
+                            edition: 1,
+                            image: "https://mx.salvat.com/colecciones/ford-mustang/images/entrega1-crop-u2798143.png",
+                            description: "Fascículo + Capó delantero",
+                        },
+                        {
+                            edition: 2,
+                            image: "https://mx.salvat.com/colecciones/ford-mustang/images/entrega2-crop-u2798223.png",
+                            description: "Fascículo + Primera rueda",
+                        },
+                        {
+                            edition: 3,
+                            image: "https://mx.salvat.com/colecciones/ford-mustang/images/entrega3-crop-u2798293.png",
+                            description: "Fascículo + La culata",
+                        },
+                        {
+                            edition: 4,
+                            image: "https://mx.salvat.com/colecciones/ford-mustang/images/entrega4-crop-u2798363.png",
+                            description: "Fascículo + El filtro de aire",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "Planeta",
+            items: [
+                {
+                    name: "Planeta Metal",
+                    cover: "",
+                    editions: [
+                        {
+                            description: "AC/DC",
+                            image: "https://mx.salvat.com/colecciones/metal-planet/images/entrega4.png",
+                            code: "1",
+                            edition: "1",
+                        },
+                        {
+                            description: "Metallica",
+                            image: "https://mx.salvat.com/colecciones/metal-planet/images/entrega2.png",
+                            code: "2",
+                            edition: "2",
+                        },
+                        {
+                            description: "Iron Maiden",
+                            image: "https://mx.salvat.com/colecciones/metal-planet/images/entrega1.png",
+                            code: "3",
+                            edition: "3",
+                        },
+                        {
+                            id: 4321,
+                            description: "Led Zeppelin",
+                            image: "https://mx.salvat.com/colecciones/metal-planet/images/entreg3.png",
+                            code: "4",
+                            edition: "4",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "Loopa",
+            items: [
+                {
+                    name: "Formula 1",
+                    cover: "",
+                    editions: [
+                        {
+                            description: "Carroceria",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_629925-MLA77235560904_072024-F.webp",
+                            code: "",
+                            edition: "45",
+                        },
+                        {
+                            description: "",
+                            image: "",
+                            code: "",
+                            edition: "46",
+                        },
+                        {
+                            description: "",
+                            image: "",
+                            code: "",
+                            edition: "47",
+                        },
+                    ],
+                },
+                {
+                    name: "Formula 1 redbull",
+                    cover: "",
+                    editions: [
+                        {
+                            description: "",
+                            image: "",
+                            code: "",
+                            edition: "1",
+                        },
+                        {
+                            description: "",
+                            image: "",
+                            code: "",
+                            edition: "2",
+                        },
+                        {
+                            description: "",
+                            image: "",
+                            code: "",
+                            edition: "3",
+                        },
+                    ],
+                },
+                {
+                    name: "Redbull",
+                    cover: "",
+                    editions: [
+                        {
+                            code: "",
+                            description: "",
+                            image: "",
+                            edition: "23",
+                        },
+                        {
+                            code: "",
+                            description: "",
+                            image: "",
+                            edition: "24",
+                        },
+                        {
+                            code: "",
+                            description: "",
+                            image: "",
+                            edition: "25",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "Prueba",
+            cover: "",
+            items: [
+                {
+                    name: "Lala",
+                    editions: [
+                        {
+                            description: "Yogurt",
+                            image: "https://pasajinak.com/wp-content/uploads/Yogurt-de-Fresa-1-litro-1.jpg",
+                            code: "7502261257955",
+                            edition: "23",
+                        },
+                    ],
+                },
+                {
+                    name: "Spicy Nuts",
+                    editions: [
+                        {
+                            description: "Cacahuates crujientes con cubierta de chile sabor limon",
+                            image: "https://www.tiendas3b.com/wp-content/uploads/2025/05/KONICHIWA_Spicy_150g_1.jpg",
+                            code: "7502261268708",
+                            edition: "56",
+                        },
+                    ],
+                },
+                {
+                    name: "Grate Value Malvabiscos",
+                    editions: [
+                        {
+                            description: "Bombones de forma de corazon",
+                            image: "https://i5-mx.walmartimages.com/gr/images/product-images/img_large/00750179164996L.jpg",
+                            code: "7506495071219",
+                            edition: "345",
+                        },
+                    ],
+                },
+                {
+                    name: "Rellenitos",
+                    editions: [
+                        {
+                            description: "Panquesitos rellenos de chocolate",
+                            image: "https://http2.mlstatic.com/D_NQ_NP_2X_788306-MLM87728820043_072025-F.webp",
+                            code: "7502261269255",
+                            edition: "345",
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
---
+++ src/js/index.js
@@ -0,0 +1,193 @@
+import { db } from "./db.js";
+
+console.log(db.editoriales);
+const $d = document;
+const selector = (tag, ref = $d) => ref.querySelector(`${tag}`);
+const selectorAll = (tag, ref = $d) => ref.querySelectorAll(`${tag}`);
+const themeBtn = selector("[theme-btn]");
+const pageBody = selector("body");
+
+const menuBtns = selectorAll("[menu-btn]");
+const sectionWindows = selectorAll("[window]");
+const noImgLink = "/src/img/no_img.jpg";
+const scannerConfig = {
+    fps: 10,
+    qrbox: { width: 250, height: 250 },
+    formatsToSupport: [
+        Html5QrcodeSupportedFormats.QR_CODE,
+        Html5QrcodeSupportedFormats.EAN_13,
+        Html5QrcodeSupportedFormats.ITF,
+        Html5QrcodeSupportedFormats.UPC_A,
+        Html5QrcodeSupportedFormats.UPC_E,
+        Html5QrcodeSupportedFormats.RSS_14,
+        Html5QrcodeSupportedFormats.CODE_39,
+        Html5QrcodeSupportedFormats.CODE_93,
+        Html5QrcodeSupportedFormats.CODE_128,
+        Html5QrcodeSupportedFormats.RSS_EXPANDED,
+    ],
+};
+const deleteChildElements = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while (child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const sanitizeInput = (inputValue) => {
+    const div = document.createElement("div");
+    div.textContent = inputValue;
+    return div.innerHTML;
+};
+const resetWindows = () => {
+    menuBtns.forEach((btn) => btn.setAttribute("active-state", "false"));
+    sectionWindows.forEach((window) => {
+        window.setAttribute("show", "false");
+        window.setAttribute("visible", "false");
+    });
+};
+
+const html5QrCode = new Html5Qrcode("reader");
+const showEditionData = (itemName, data) => {
+    const resultWindow = selector('[window="result"]');
+    resultWindow.setAttribute("show", "true");
+    setTimeout(() => {
+        resultWindow.setAttribute("visible", "true");
+    }, 100);
+    const name = selector('[result="name"]');
+    const description = selector('[result="description"]');
+    const image = selector('[result="image"]');
+    const ed = selector('[result="ed"]');
+    const code = selector('[result="code"]');
+    name.textContent = itemName;
+    description.textContent = data.description;
+    ed.textContent = data.ed;
+    code.textContent = data.code;
+    image.src = data.image;
+};
+const onScanSuccess = (decodedText, decodedResult) => {
+    console.log(decodedText);
+
+    let itemSearched;
+    /* selector("selector='scan_'").textContent = `${decodedText}`; */
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+    db.editoriales.forEach((editorial) => {
+        editorial.items.forEach((item) => {
+            item.editions.forEach((edition) => {
+                console.log(edition);
+                showEditionData(item.name, edition);
+            });
+        });
+    });
+    html5QrCode.stop();
+    resetWindows();
+};
+
+const changeTheme = () => {
+    const state = pageBody.getAttribute("color-scheme");
+    selectorAll(`[theme-icon]`).forEach((icon) => {
+        icon.getAttribute("theme-icon") === state ? icon.setAttribute("show", "false") : icon.setAttribute("show", "true");
+    });
+    pageBody.setAttribute("color-scheme", pageBody.getAttribute("color-scheme") === "dark" ? "light" : "dark");
+};
+themeBtn.addEventListener("click", changeTheme);
+
+menuBtns.forEach((menuBtn) => {
+    menuBtn.addEventListener("click", () => {
+        const target = menuBtn.getAttribute("menu-btn");
+        const currentState = menuBtn.getAttribute("active-state");
+        if (currentState !== "true") {
+            resetWindows();
+
+            if (target === "scan") {
+                html5QrCode.start({ facingMode: "environment" }, scannerConfig, onScanSuccess);
+            } else {
+                if (html5QrCode.isScanning === true) {
+                    html5QrCode.stop();
+                }
+            }
+
+            const targetWindow = selector(`[window="${target}"]`);
+
+            switch (currentState) {
+                case "true":
+                    targetWindow.setAttribute("visible", "false");
+                    setTimeout(() => {
+                        targetWindow.setAttribute("show", "false");
+                    }, 500);
+                    break;
+                case "false":
+                    targetWindow.setAttribute("show", "true");
+                    setTimeout(() => {
+                        targetWindow.setAttribute("visible", "true");
+                    }, 100);
+                    break;
+            }
+            menuBtn.setAttribute("active-state", currentState === "true" ? "false" : "true");
+        }
+    });
+});
+const menuActionBtns = selectorAll("[action-menu]");
+const serachResult = selector('[selector="search_result"]');
+
+menuActionBtns.forEach((menuActionBtn) => {
+    menuActionBtn.addEventListener("click", () => {
+        const currentState = menuActionBtn.getAttribute("active-state");
+        const target = menuActionBtn.getAttribute("action-menu");
+        const targetMenu = selector(`[menu-container="${target}"]`);
+        switch (currentState) {
+            case "true":
+                targetMenu.setAttribute("visible", "false");
+                setTimeout(() => {
+                    targetMenu.setAttribute("show", "false");
+                }, 500);
+                break;
+            case "false":
+                targetMenu.setAttribute("show", "true");
+                setTimeout(() => {
+                    targetMenu.setAttribute("visible", "true");
+                }, 100);
+                break;
+        }
+        menuActionBtn.setAttribute("active-state", currentState === "true" ? "false" : "true");
+    });
+});
+
+const setSearchResult = (item) => {
+    console.log(item);
+};
+
+const searchBtn = selector("[action-btn='search']");
+const itemCardTemplate = selector("[template-ref='item_card']").content;
+const editionCardTemplate = selector("[template-ref='edition_card']").content;
+const createEditionCard = (data, itemName) => {
+    /* console.log(data); */
+    const newEdition = editionCardTemplate.cloneNode(true);
+    const card = selector('[selector="card"]', newEdition);
+    const edition = selector('[selector="edition_number"]', newEdition);
+    const code = selector('[selector="edition_code"]', newEdition);
+    const name = selector('[selector="edition_name"]', newEdition);
+    const image = selector('[selector="edition_image"]', newEdition);
+    const editorialImage = selector('[selector="edition_editorial_image"]', newEdition);
+    code.textContent = data.code;
+    edition.textContent = data.edition;
+    name.textContent = itemName;
+    /* console.log(data.image); */
+    image.src = data.image === "" ? noImgLink : data.image;
+    return newEdition;
+};
+const setSearchStartCards = () => {
+    const resultContainer = selector('[cards-container="results"]');
+    deleteChildElements(resultContainer);
+    db.editoriales.forEach((editorial) => {
+        editorial.items.forEach((item) => {
+            /* console.log(item); */
+            item.editions.forEach((edition) => {
+                /* console.log(edition); */
+                resultContainer.appendChild(createEditionCard(edition, item.name));
+            });
+        });
+    });
+};
+setSearchStartCards();
+const searchInput = selector('[selector="search_input"]');
